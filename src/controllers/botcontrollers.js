4890650a5888dc4d018152970b2ceb536c58e4af
Mac Fitton <marcellofitton@gmail.com>
2022-11-17 08:37:43 -0800

    Implemented search matching and refactored embedded message to change on conditions

--- src/controllers/botcontrollers.js
+++ src/controllers/botcontrollers.js
@@ -95,37 +95,34 @@ const botControllers =  {
               const res = await axios.get(
                 `https://www.omdbapi.com/?t=${title.split(' ').join('_')}&apikey=272fc884`
               );
-    
+              const {Year: year, Director: director, Language: language, Runtime: runtime, Metascore: metascore, imdbRating} = res.data
               const movieEmbed = new EmbedBuilder()
-                .setColor(0x20124d)
-                .setTitle(title || "N/A")
-                .setDescription(res.data.Plot || "N/A")
-                .setImage(res.data.Poster || null)
-                .addFields(
-                  { name: "Year", value: res.data.Year || "N/A", inline: true },
-                  {
-                    name: "Director",
-                    value: res.data.Director || "N/A",
-                    inline: true,
-                  },
-                  {
-                    name: "Language",
-                    value: res.data.Language || "N/A",
-                    inline: true,
-                  },
-                  {
-                    name: "Runtime",
-                    value: res.data.Runtime || "N/A",
-                    inline: true,
-                  }
-                )
-                .setURL(link || null);
+            .setThumbnail(imdbRating >= 8 ? 'https://i.imgur.com/pIfdoIW.gif' : null)
+            .setColor(imdbRating >= 8 ? 0xd4af37 : 0x20124d)
+            .setTitle(imdbRating >= 8 ?`🥇${title}🥇` : title || "N/A")
+            .setDescription(res.data.Plot || "N/A")
+            .setImage(res.data.Poster || null)
+            .addFields(
+              { name: "Year", value: year || "N/A", inline: true },
+              { name: "Director", value: director || "N/A", inline: true },
+              { name: "Language", value: language || "N/A", inline: true },
+              { name: "Runtime", value: runtime || "N/A", inline: true },
+              { name: "Metascore", value: metascore >= 8 ? `${metascore}` : metascore || "N/A", inline: true },
+              { name: "IMDB Rating", value: imdbRating >= 8 ? `${imdbRating}` : imdbRating || "N/A", inline: true }
+            )
+            .setURL(link || null);
                     movieArray.push(movieEmbed)
-                    console.log(movieArray);
-                    interaction.channel.send({ embeds: [movieEmbed] });
-              
+                    
+                    // interaction.channel.send({ embeds: [movieEmbed] });
+                    
+                    if(movieArray.length < 5){
+                      return 
+                    } else{
+                      interaction.reply({ embeds: movieArray})
+                    }
             });
 
+            
             
         } catch (error) {
           console.log(error);
@@ -136,21 +133,27 @@ const botControllers =  {
         const movieQuery = titleCase(interaction.options.getString('title'))
         
         try {
-          const movie = await Movie.find({ title: movieQuery });
+          const regex = new RegExp(movieQuery, 'i')
+          const movie = await Movie.find({ title: {$regex: regex} });
+          console.log(movie)
           const res = await axios.get(
             `https://www.omdbapi.com/?t=${movie[0].title}&apikey=272fc884`
           );
+          const {Year: year, Director: director, Language: language, Runtime: runtime, Metascore: metascore, imdbRating} = res.data
           console.log(res.data);
           const movieEmbed = new EmbedBuilder()
-            .setColor(0x20124d)
-            .setTitle(movie[0].title || "N/A")
+            .setThumbnail(imdbRating >= 8 ? 'https://i.imgur.com/pIfdoIW.gif' : null)
+            .setColor(imdbRating >= 8 ? 0xd4af37 : 0x20124d)
+            .setTitle(imdbRating >= 8 ?`🥇${movie[0].title}🥇` : movie[0].title || "N/A")
             .setDescription(res.data.Plot || "N/A")
             .setImage(res.data.Poster || null)
             .addFields(
-              { name: "Year", value: res.data.Year || "N/A", inline: true },
-              { name: "Director", value: res.data.Director || "N/A", inline: true },
-              { name: "Language", value: res.data.Language || "N/A", inline: true },
-              { name: "Runtime", value: res.data.Runtime || "N/A", inline: true }
+              { name: "Year", value: year || "N/A", inline: true },
+              { name: "Director", value: director || "N/A", inline: true },
+              { name: "Language", value: language || "N/A", inline: true },
+              { name: "Runtime", value: runtime || "N/A", inline: true },
+              { name: "Metascore", value: metascore >= 8 ? `${metascore}` : metascore || "N/A", inline: true },
+              { name: "IMDB Rating", value: imdbRating >= 8 ? `${imdbRating}` : imdbRating || "N/A", inline: true }
             )
             .setURL(movie[0].link || null);
     
